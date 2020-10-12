b966744a77de998209a22b6db8270e99254c2776
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-10-12 14:45:13 -0500

    firebase auth/db/storage initial

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCYtOZNI2CdYutbf1whC7nlA7shiD-6z80",
+  authDomain: "react-slack-clone-41b77.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-41b77.firebaseio.com",
+  projectId: "react-slack-clone-41b77",
+  storageBucket: "react-slack-clone-41b77.appspot.com",
+  messagingSenderId: "873523103931",
+  appId: "1:873523103931:web:39b3f9402e41cd31b6819e",
+  measurementId: "G-RNY77H6MS3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
