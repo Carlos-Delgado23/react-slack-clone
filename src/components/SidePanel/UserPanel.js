e8dc0218f81c4957d7c4deaace94a581b1949ff3
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-10-22 15:55:07 -0500

    Add Signout Functionality and Clear User Action

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import frirebase from '../../firebase'
 import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import firebase from '../../firebase';
 
 class UserPanel extends React.Component {
   dropdownOptions = () => [
@@ -14,9 +16,16 @@ class UserPanel extends React.Component {
     },
     {
       key: 'signout',
-      text: <span>Sign Out</span>
+      text: <span onClick={this.handleSignout}>Sign Out</span>
     }
-  ]
+  ];
+
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log('signed out!'))
+  }
 
   render() {
     return (
