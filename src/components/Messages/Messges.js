12df68b0e3d4941a7c87d9b7c0df24a0b562a530
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-11-30 19:05:16 -0600

    Scaffold ColorPanel / Message Components

--- src/components/Messages/Messges.js
+++ src/components/Messages/Messges.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
+import MessagesHeader from './MessagesHeader'
+import MessageForm from './MessageForm'
+
+import { Segment, Comment } from 'semantic-ui-react'
+
 class Messages extends React.Component {
   render() {
     return (
-      <div>
-        Messages
-      </div>
+      <>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">
+            {/* Messages */}
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </>
     )
   }
 }
